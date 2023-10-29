2e1ffcc290937caf7dce18dc80d644284726f855
Akmaral <dzhanyshbekova@gmail.com>
2023-10-30 04:54:45 +0600

    EditGraph

--- GSSM-ver1/GSSM-ver1/Graph_dzhanyshbekova_akmaral_PI_3_21.Designer.vb
+++ GSSM-ver1/GSSM-ver1/Graph_dzhanyshbekova_akmaral_PI_3_21.Designer.vb
@@ -22,9 +22,37 @@ Partial Class Graph_dzhanyshbekova_akmaral_PI_3_21
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Me.Chart1.Dock = System.Windows.Forms.DockStyle.Fill
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(0, 0)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(800, 450)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'Graph_dzhanyshbekova_akmaral_PI_3_21
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Chart1)
+        Me.Name = "Graph_dzhanyshbekova_akmaral_PI_3_21"
         Me.Text = "Graph_dzhanyshbekova_akmaral_PI_3_21"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
 End Class
--- GSSM-ver1/GSSM-ver1/Graph_dzhanyshbekova_akmaral_PI_3_21.vb
+++ GSSM-ver1/GSSM-ver1/Graph_dzhanyshbekova_akmaral_PI_3_21.vb
@@ -1,3 +1,68 @@
 ﻿Public Class Graph_dzhanyshbekova_akmaral_PI_3_21
+    Private Sub Graph_dzhanyshbekova_akmaral_PI_3_21_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Устанавливаем диапазон значений на оси X и Y
 
+        Chart1.ChartAreas(0).AxisX.Minimum = -1.5
+        Chart1.ChartAreas(0).AxisX.Maximum = 4
+        Chart1.ChartAreas(0).AxisY.Maximum = 1
+        Chart1.ChartAreas(0).AxisY.Minimum = -0.1
+        ' Установите интервал мажорных делений для оси X
+        Chart1.ChartAreas(0).AxisX.Interval = 0.5 ' Например, устанавливаем интервал в 1
+
+        ' Установите интервал мажорных делений для оси Y
+        Chart1.ChartAreas(0).AxisY.Interval = 0.1 ' Например, устанавливаем интервал в 0.2
+
+        ' Добавляем серию для графика
+        Dim series As New DataVisualization.Charting.Series()
+        series.ChartType = DataVisualization.Charting.SeriesChartType.Line
+        series.Name = "y = (x^2/2-x^3/6)^2"
+
+        ' Добавляем точки на график, вычисляя значения функции
+        For x As Double = -3 To 5 Step 0.01
+            Dim y As Double = (x * x / 2 - x * x * x / 6) ^ 2
+            series.Points.AddXY(x, y)
+        Next
+
+        ' Добавляем серию на график
+        Chart1.Series.Add(series)
+        series.BorderWidth = 4
+
+
+        Dim intersectionX As Double = 0
+        Dim intersectionY As Double = 0
+
+        ' Добавляем серию для прямой x = 0
+        Dim lineX As New DataVisualization.Charting.Series()
+        lineX.ChartType = DataVisualization.Charting.SeriesChartType.Line
+        lineX.Name = "X Line"
+        lineX.Points.AddXY(0, -0.4)
+        lineX.Points.AddXY(0, 2)
+        ' Настройка стиля и цвета линии
+        lineX.Color = Color.Black
+        lineX.BorderWidth = 2
+        Chart1.Series.Add(lineX)
+
+        ' Добавляем серию для прямой y = 0
+        Dim lineY As New DataVisualization.Charting.Series()
+        lineY.ChartType = DataVisualization.Charting.SeriesChartType.Line
+        lineY.Name = "Y Line"
+        lineY.Points.AddXY(-2, 0)
+        lineY.Points.AddXY(4, 0)
+        ' Настройка стиля и цвета линии
+        lineY.Color = Color.Black
+        lineY.BorderWidth = 2
+        Chart1.Series.Add(lineY)
+
+        Chart1.ChartAreas(0).AxisX.MajorTickMark.Enabled = True
+        Chart1.ChartAreas(0).AxisY.MajorTickMark.Enabled = True
+
+        ' Настройка внешнего вида главных линий и делений сетки (по вашим предпочтениям)
+        Chart1.ChartAreas(0).AxisX.MajorGrid.LineColor = Color.LightGray
+        Chart1.ChartAreas(0).AxisY.MajorGrid.LineColor = Color.LightGray
+        Chart1.ChartAreas(0).AxisX.MajorTickMark.LineColor = Color.LightGray
+        Chart1.ChartAreas(0).AxisY.MajorTickMark.LineColor = Color.LightGray
+        Chart1.ChartAreas(0).AxisX.MajorTickMark.Size = 1
+        Chart1.ChartAreas(0).AxisY.MajorTickMark.Size = 1
+
+    End Sub
 End Class
