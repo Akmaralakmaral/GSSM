75ccaf795bcc25cf452f4344d6267cd857009ecf
Akmaral <dzhanyshbekova@gmail.com>
2023-10-30 04:45:31 +0600

    GSSMреализация

--- GSSM-ver1/GSSM-ver1/Form1.Designer.vb
+++ GSSM-ver1/GSSM-ver1/Form1.Designer.vb
@@ -22,10 +22,360 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(8, 101)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(308, 20)
+        Me.TextBox6.TabIndex = 13
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(82, 370)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 44)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Run ESM"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(79, 52)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(88, 13)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "(X^2/2-X^3/6)^2"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(101, 325)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(69, 17)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Maximum"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(101, 291)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(66, 17)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Minimum"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(8, 274)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Search parametr:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(10, 227)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 211)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Limit of iteration:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(8, 163)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(308, 20)
+        Me.TextBox7.TabIndex = 11
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(6, 147)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(100, 13)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "Number of Iteration:"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(9, 227)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(307, 20)
+        Me.TextBox8.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(10, 163)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 147)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Tollerance:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(193, 101)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(146, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 85)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "F(X):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "LeftEndPoint(a):"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label11.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Label11.Location = New System.Drawing.Point(302, -16)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(231, 25)
+        Me.Label11.TabIndex = 5
+        Me.Label11.Text = "Even Search Method"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Function F(X):"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(151, 370)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(88, 34)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "Clear Out Put"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(9, 370)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 34)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Show Function Graph"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(8, 45)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(308, 20)
+        Me.TextBox5.TabIndex = 15
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.TextBox9)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.TextBox5)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.TextBox6)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.TextBox7)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.TextBox8)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Location = New System.Drawing.Point(388, 30)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(350, 436)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Output Data"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Root is X:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(9, 274)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(97, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Elapsed Time(sec):"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 30)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(345, 436)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input Data"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 101)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(146, 20)
+        Me.TextBox1.TabIndex = 14
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(200, 85)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(90, 13)
+        Me.Label12.TabIndex = 13
+        Me.Label12.Text = "RightEndPoint(b):"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(8, 291)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(307, 20)
+        Me.TextBox9.TabIndex = 18
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(9, 211)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(48, 13)
+        Me.Label13.TabIndex = 19
+        Me.Label13.Text = "abs(b-a):"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(839, 499)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents TextBox9 As TextBox
 End Class
--- GSSM-ver1/GSSM-ver1/Form1.vb
+++ GSSM-ver1/GSSM-ver1/Form1.vb
@@ -1,3 +1,155 @@
-﻿Public Class Form1
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
+Public Class Form1
+
+    Dim stopwatch As New Stopwatch()
+
+    Public Sub New()
+        ' Этот вызов является обязательным для конструктора.
+        InitializeComponent()
+
+        ' Добавьте здесь код для изменения культуры.
+        System.Threading.Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo("en-US")
+    End Sub
+    Private Function CalculateFunction(x As Decimal) As Decimal
+        ' Функция f(x)
+        Return (x * x / 2 - x * x * x / 6) ^ 2
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim a As Double = Double.Parse(TextBox1.Text)
+            Dim b As Double = Double.Parse(TextBox2.Text)
+            Dim tol As Double = Double.Parse(TextBox3.Text)
+            Dim k_max As Integer = Integer.Parse(TextBox4.Text)
+            Dim cond As Integer = 0
+
+
+            Dim r As Double = (Math.Sqrt(5) - 1) / 2
+            Dim x1 As Double = a + (1 - r) * (b - a)
+            Dim f1 As Double = CalculateFunction(x1)
+            Dim x2 As Double = a + r * (b - a)
+            Dim f2 As Double = CalculateFunction(x2)
+            Dim k As Integer = 0
+
+
+
+            Try
+
+                'минимум ===================================================================
+
+                If (RadioButton1.Checked) Then
+                    stopwatch.Start()
+                    While cond = 0
+
+
+                        While k < k_max And (b - a) > tol
+                            If f1 > f2 Then
+                                a = x1
+                                x1 = x2
+                                f1 = f2
+                                x2 = a + r * (b - a)
+                                f2 = CalculateFunction(x2)
+
+                            Else
+                                b = x2
+                                x2 = x1
+                                f2 = f1
+                                x1 = a + (1 - r) * (b - a)
+                                f1 = CalculateFunction(x1)
+                            End If
+
+
+                            k += 1
+                        End While
+                        cond = 2
+                    End While
+                    Dim minimumValue As Double = Math.Abs(b - a)
+                    TextBox5.Text = x2.ToString()
+                    TextBox6.Text = f2.ToString()
+                    TextBox7.Text = k.ToString()
+                    TextBox8.Text = minimumValue.ToString()
+                    k = 0
+                    x1 = 0
+                    x2 = 0
+                    f1 = 0
+                    f2 = 0
+                    cond = 0
+
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+            End Try
+            stopwatch.Stop()
+            Dim elapsedSecondsMin As Double = stopwatch.Elapsed.TotalSeconds
+            TextBox9.Text = elapsedSecondsMin.ToString()
+
+
+
+
+
+
+            Try
+                'максимум ===================================================================
+
+                If (RadioButton2.Checked) Then
+                    stopwatch.Start()
+                    While cond = 0
+
+
+                        While k < k_max And (b - a) > tol
+                            If f1 < f2 Then
+                                a = x1
+                                x1 = x2
+                                f1 = f2
+                                x2 = a + r * (b - a)
+                                f2 = CalculateFunction(x2)
+
+                            Else
+                                b = x2
+                                x2 = x1
+                                f2 = f1
+                                x1 = a + (1 - r) * (b - a)
+                                f1 = CalculateFunction(x1)
+                            End If
+
+
+                            k += 1
+                        End While
+                        cond = 2
+                    End While
+                    Dim minimumValue As Double = Math.Abs(b - a)
+                    TextBox5.Text = x2.ToString()
+                    TextBox6.Text = f2.ToString()
+                    TextBox7.Text = k.ToString()
+                    TextBox8.Text = minimumValue.ToString()
+                    k = 0
+                    x1 = 0
+                    x2 = 0
+                    f1 = 0
+                    f2 = 0
+                    cond = 0
+
+
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+            End Try
+            stopwatch.Stop()
+            Dim elapsedSecondsMax As Double = stopwatch.Elapsed.TotalSeconds
+            TextBox8.Text = elapsedSecondsMax.ToString()
+            stopwatch.Reset()
+
+        Catch ex As Exception
+            MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+        End Try
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+    End Sub
 End Class
