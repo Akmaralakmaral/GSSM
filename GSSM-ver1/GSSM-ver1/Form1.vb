cfea55ef26d32011939a930c11b2555119ad235a
Akmaral <dzhanyshbekova@gmail.com>
2023-11-13 04:29:59 +0600

    calculate

--- GSSM-ver1/GSSM-ver1/Form1.vb
+++ GSSM-ver1/GSSM-ver1/Form1.vb
@@ -5,10 +5,7 @@ Public Class Form1
     Dim stopwatch As New Stopwatch()
 
     Public Sub New()
-        ' Этот вызов является обязательным для конструктора.
         InitializeComponent()
-
-        ' Добавьте здесь код для изменения культуры.
         System.Threading.Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo("en-US")
     End Sub
     Private Function CalculateFunction(x As Decimal) As Decimal
@@ -16,132 +13,159 @@ Public Class Form1
         Return (x * x / 2 - x * x * x / 6) ^ 2
     End Function
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
         Try
-            Dim a As Decimal = Decimal.Parse(TextBox1.Text)
-            Dim b As Decimal = Decimal.Parse(TextBox2.Text)
-            Dim tol As Decimal = Decimal.Parse(TextBox3.Text)
-            Dim k_max As Integer = Integer.Parse(TextBox4.Text)
-            Dim cond As Integer = 0
+            If (ComboBox1.Text = "" Or TextBox2.Text = "" _
+                Or TextBox3.Text = "" Or TextBox4.Text = "") Then
+                MsgBox("Текстовые поля ввода пусты! Введите данные")
+            ElseIf Not (IsNumeric(TextBox2.Text) And IsNumeric(TextBox3.Text) And IsNumeric(TextBox4.Text)) Then
+                MsgBox("Введите только числовые значения")
 
+            Else
+                Dim GSSM As GSSM_module = New GSSM_module()
 
-            Dim r As Decimal = (Math.Sqrt(5) - 1) / 2
-            Dim x1 As Decimal = a + (1 - r) * (b - a)
-            Dim f1 As Decimal = CalculateFunction(x1)
-            Dim x2 As Decimal = a + r * (b - a)
-            Dim f2 As Decimal = CalculateFunction(x2)
-            Dim k As Integer = 0
+                GSSM.input(ComboBox1, TextBox1, TextBox2, TextBox3,
+                  TextBox4, TextBox14, TextBox13, RadioButton1,
+                  RadioButton2, TextBox10)
 
-
-
-            Try
-
-                'минимум ===================================================================
-
-                If (RadioButton1.Checked) Then
-                    stopwatch.Start()
-                    While cond = 0
-
-
-                        While k < k_max And (b - a) > tol
-                            If f1 > f2 Then
-                                a = x1
-                                x1 = x2
-                                f1 = f2
-                                x2 = a + r * (b - a)
-                                f2 = CalculateFunction(x2)
-
-                            Else
-                                b = x2
-                                x2 = x1
-                                f2 = f1
-                                x1 = a + (1 - r) * (b - a)
-                                f1 = CalculateFunction(x1)
-                            End If
-
-
-                            k += 1
-                        End While
-                        cond = 2
-                    End While
-                    Dim minimumValue As Decimal = Math.Abs(b - a)
-                    TextBox5.Text = x2.ToString()
-                    TextBox6.Text = f2.ToString()
-                    TextBox7.Text = k.ToString()
-                    TextBox8.Text = minimumValue.ToString()
-                    k = 0
-                    x1 = 0
-                    x2 = 0
-                    f1 = 0
-                    f2 = 0
-                    cond = 0
-
-                End If
-            Catch ex As Exception
-                MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
-            End Try
-            stopwatch.Stop()
-            Dim elapsedSecondsMin As Double = stopwatch.Elapsed.TotalSeconds
-            TextBox9.Text = elapsedSecondsMin.ToString()
-
-
-
-
-
-
-            Try
-                'максимум ===================================================================
-
-                If (RadioButton2.Checked) Then
-                    stopwatch.Start()
-                    While cond = 0
-
-
-                        While k < k_max And (b - a) > tol
-                            If f1 < f2 Then
-                                a = x1
-                                x1 = x2
-                                f1 = f2
-                                x2 = a + r * (b - a)
-                                f2 = CalculateFunction(x2)
-
-                            Else
-                                b = x2
-                                x2 = x1
-                                f2 = f1
-                                x1 = a + (1 - r) * (b - a)
-                                f1 = CalculateFunction(x1)
-                            End If
-
-
-                            k += 1
-                        End While
-                        cond = 2
-                    End While
-                    Dim minimumValue As Decimal = Math.Abs(b - a)
-                    TextBox5.Text = x2.ToString()
-                    TextBox6.Text = f2.ToString()
-                    TextBox7.Text = k.ToString()
-                    TextBox8.Text = minimumValue.ToString()
-                    k = 0
-                    x1 = 0
-                    x2 = 0
-                    f1 = 0
-                    f2 = 0
-                    cond = 0
-
-
-                End If
-            Catch ex As Exception
-                MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
-            End Try
-            stopwatch.Stop()
-            Dim elapsedSecondsMax As Double = stopwatch.Elapsed.TotalSeconds
-            TextBox9.Text = elapsedSecondsMax.ToString()
-            stopwatch.Reset()
-
-        Catch ex As Exception
-            MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+                GSSM.output(TextBox11, TextBox12, TextBox5, TextBox6, TextBox7, TextBox9, TextBox14, TextBox13, TextBox10)
+            End If
+        Catch ef As FormatException
+            MsgBox("Ошибка в формате входных данных")
         End Try
+
+
+
+
+
+
+
+
+
+        'Try
+        '    Dim a As Decimal = Decimal.Parse(TextBox1.Text)
+        '    Dim b As Decimal = Decimal.Parse(TextBox2.Text)
+        '    Dim tol As Decimal = Decimal.Parse(TextBox3.Text)
+        '    Dim k_max As Integer = Integer.Parse(TextBox4.Text)
+        '    Dim cond As Integer = 0
+
+
+        '    Dim r As Decimal = (Math.Sqrt(5) - 1) / 2
+        '    Dim x1 As Decimal = a + (1 - r) * (b - a)
+        '    Dim f1 As Decimal = CalculateFunction(x1)
+        '    Dim x2 As Decimal = a + r * (b - a)
+        '    Dim f2 As Decimal = CalculateFunction(x2)
+        '    Dim k As Integer = 0
+
+
+
+        '    Try
+
+        '        'минимум ===================================================================
+
+        '        If (RadioButton1.Checked) Then
+        '            stopwatch.Start()
+        '            While cond = 0
+
+
+        '                While k < k_max And (b - a) > tol
+        '                    If f1 > f2 Then
+        '                        a = x1
+        '                        x1 = x2
+        '                        f1 = f2
+        '                        x2 = a + r * (b - a)
+        '                        f2 = CalculateFunction(x2)
+
+        '                    Else
+        '                        b = x2
+        '                        x2 = x1
+        '                        f2 = f1
+        '                        x1 = a + (1 - r) * (b - a)
+        '                        f1 = CalculateFunction(x1)
+        '                    End If
+        '                    k += 1
+        '                End While
+        '                cond = 2
+        '            End While
+        '            Dim minimumValue As Decimal = Math.Abs(b - a)
+        '            TextBox5.Text = x2.ToString()
+        '            TextBox6.Text = f2.ToString()
+        '            TextBox7.Text = k.ToString()
+        '            TextBox8.Text = minimumValue.ToString()
+        '            k = 0
+        '            x1 = 0
+        '            x2 = 0
+        '            f1 = 0
+        '            f2 = 0
+        '            cond = 0
+
+        '        End If
+        '    Catch ex As Exception
+        '        MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+        '    End Try
+        '    stopwatch.Stop()
+        '    Dim elapsedSecondsMin As Double = stopwatch.Elapsed.TotalSeconds
+        '    TextBox9.Text = elapsedSecondsMin.ToString()
+
+
+
+
+
+
+        '    Try
+        '        'максимум ===================================================================
+
+        '        If (RadioButton2.Checked) Then
+        '            stopwatch.Start()
+        '            While cond = 0
+
+
+        '                While k < k_max And (b - a) > tol
+        '                    If f1 < f2 Then
+        '                        a = x1
+        '                        x1 = x2
+        '                        f1 = f2
+        '                        x2 = a + r * (b - a)
+        '                        f2 = CalculateFunction(x2)
+
+        '                    Else
+        '                        b = x2
+        '                        x2 = x1
+        '                        f2 = f1
+        '                        x1 = a + (1 - r) * (b - a)
+        '                        f1 = CalculateFunction(x1)
+        '                    End If
+
+
+        '                    k += 1
+        '                End While
+        '                cond = 2
+        '            End While
+        '            Dim minimumValue As Decimal = Math.Abs(b - a)
+        '            TextBox5.Text = x2.ToString()
+        '            TextBox6.Text = f2.ToString()
+        '            TextBox7.Text = k.ToString()
+        '            TextBox8.Text = minimumValue.ToString()
+        '            k = 0
+        '            x1 = 0
+        '            x2 = 0
+        '            f1 = 0
+        '            f2 = 0
+        '            cond = 0
+
+
+        '        End If
+        '    Catch ex As Exception
+        '        MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+        '    End Try
+        '    stopwatch.Stop()
+        '    Dim elapsedSecondsMax As Double = stopwatch.Elapsed.TotalSeconds
+        '    TextBox9.Text = elapsedSecondsMax.ToString()
+        '    stopwatch.Reset()
+
+        'Catch ex As Exception
+        '    MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+        'End Try
 
 
     End Sub
@@ -153,7 +177,4 @@ Public Class Form1
         TextBox8.Text = ""
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Graph_dzhanyshbekova_akmaral_PI_3_21.Show()
-    End Sub
 End Class
---
+++ GSSM-ver1/GSSM-ver1/GSSM-module.vb
@@ -0,0 +1,255 @@
+﻿Imports System.Math
+Imports org.mariuszgromada.math.mxparser
+Imports aziretParser
+Imports System.Windows.Forms.VisualStyles
+
+
+Public Class GSSM_module
+    Declare Function GetCurrentProcessId Lib "kernel32" () As Long
+    Dim Func As String
+    Dim cond As Integer
+    Dim stopwatch As New Stopwatch()
+    Dim elapsedSeconds As Double
+    Dim SolutionOfTask As Decimal
+    Dim ValueOfFunction As Decimal
+    Dim FinalInaccuracy As Decimal
+    Dim Fx1 As Decimal
+    Dim Fx2 As Decimal
+    Dim k As Integer
+
+    Function Fd(a As Decimal, Search_Step As Decimal) As Decimal
+
+        If Search_Step > 0.000000000000001 Then
+            Dim x As New Argument("x = " + a.ToString())
+            Dim parser = New Expression(Func, x)
+            Return parser.calculate
+        Else
+            Return aziretParser.ParserDecimal.Compute(Func, a)
+        End If
+    End Function
+
+    Public Sub input(funcBox As ComboBox, left As TextBox, right As TextBox,
+                     ToleranceBox As TextBox, k_maxBox As TextBox,
+                     xminusBox As TextBox, xplusBox As TextBox,
+                     RadioButton1 As RadioButton, RadioButton2 As RadioButton,
+                     TextBox1 As TextBox)
+
+        Dim H As Decimal
+        Dim f1 As Decimal
+        Dim f2 As Decimal
+        Func = funcBox.SelectedItem.ToString
+        Dim Tolerance As Decimal
+        Dim x1 As Decimal
+        Dim x2 As Decimal
+        Dim a As Decimal
+        Dim b As Decimal
+        Dim k_max As Integer
+        x1 = 0
+        H = 0
+        x2 = 0
+
+        Try
+            a = Decimal.Parse(left.Text)
+            b = Decimal.Parse(right.Text)
+            Tolerance = Double.Parse(ToleranceBox.Text)
+            H = Tolerance
+            k_max = Integer.Parse(k_maxBox.Text)
+            k = 0
+
+            Dim r As Decimal = (Math.Sqrt(5) - 1) / 2
+            x1 = a + (1 - r) * (b - a)
+            f1 = Fd(x1, H)
+            x2 = a + r * (b - a)
+            f2 = Fd(x2, H)
+
+            If k_max <= 0 Then
+                MessageBox.Show("Количество итераций должно быть не менее 1",
+                 "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ElseIf Tolerance <= 0 Then
+                MessageBox.Show("Tolerance должен быть больше нуля",
+                    "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            Else
+                If (RadioButton1.Checked) Then   'минимум 
+                    stopwatch.Start()
+
+
+                    While Math.Abs(b - a) > Tolerance And k < k_max
+                        If f1 > f2 Then
+                            a = x1
+                            x1 = x2
+                            f1 = f2
+                            x2 = a + r * (b - a)
+                            f2 = Fd(x2, H)
+                            ValueOfFunction = f2
+                            SolutionOfTask = x2
+
+                        Else
+                            b = x2
+                            x2 = x1
+                            f2 = f1
+                            x1 = a + (1 - r) * (b - a)
+                            f1 = Fd(x1, H)
+                            ValueOfFunction = f1
+                            SolutionOfTask = x1
+                        End If
+                        k += 1
+                    End While
+
+                    Fx1 = Fd(x1 - Tolerance, H)
+                    Fx2 = Fd(x1 + Tolerance, H)
+                    'If cond = 1 Then
+
+                    '    If Sign(YF1 - f1) = Sign(YF1 - f2) Then
+                    '        cond = 0
+                    '        TextBox1.ForeColor = System.Drawing.Color.Green
+                    '        TextBox1.Text = "Результат x* минимайзером этой функции, потому что" & vbCrLf &
+                    '                "Знак [F(x*)-F(x*–Tolerance)]  = " & Sign(YF1 - f1) & vbCrLf &
+                    '                "и " & vbCrLf &
+                    '                "Знак [F(x*)-F(x*+Tolerance)]  = " & Sign(YF1 - f2) & vbCrLf &
+                    '                "имеют одинаковые (минусовые) знаки" & vbCrLf &
+                    '                "Он был найден с ошибкой H = " & ToleranceBox.Text & vbCrLf &
+                    '                "Это равно заданному Tolerance!"
+
+                    '    Else
+                    '        MessageBox.Show("Задайте начальную точку X(0), так чтобы она была меньше от оптимальной точки (экстремума), для этого нажмите на кнопку Show Function Graph, чтобы увидеть график",
+                    '                        "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    '        x1 = 0
+                    '        YF1 = 0
+                    '        k = 0
+                    '        f1 = 0
+                    '        f2 = 0
+                    '        elapsedSeconds = 0
+                    '        TextBox1.Text = ""
+                    '    End If
+                    'Else
+
+                    '    If YF1 <= f1 And YF1 <= f2 Then
+                    '        TextBox1.ForeColor = System.Drawing.Color.Green
+                    '        TextBox1.Text = "Результат x* минимайзером этой функции, потому что" & vbCrLf &
+                    '                        "Знак [F(x*)—F(x*–Tolerance)]  = " & Sign(YF1 - f1) & vbCrLf &
+                    '                        "и " & vbCrLf &
+                    '                        "Знак [F(x*)-F(x*+Tolerance)]  = " & Sign(YF1 - f2) & vbCrLf &
+                    '                        "имеют одинаковые (минусовые) знаки" & vbCrLf &
+                    '                        "Он был найден с ошибкой H = " & ToleranceBox.Text & vbCrLf &
+                    '                        "Это равно заданному Tolerance!"
+                    '    Else
+                    '        TextBox1.ForeColor = System.Drawing.Color.Red
+                    '        TextBox1.Text = "Результат x* не является минимайзером этой функции, потому что" & vbCrLf &
+                    '                        "Знак [F(x*)-F(x*–Tolerance)]  = " & Sign(YF1 - f1) & vbCrLf &
+                    '                        "и " & vbCrLf &
+                    '                        "Знак [F(x*)-F(x*+Tolerance)]  = " & Sign(YF1 - f2) & vbCrLf &
+                    '                        "имеют разные знаки"
+                    '        MsgBox(" Вы должны увеличить Limit of iteration! Эта функция не нашла минимум при заданном количестве итераций")
+                    '    End If
+                    'End If
+                ElseIf (RadioButton2.Checked) Then 'максимум
+                    stopwatch.Start()
+
+                    While Math.Abs(b - a) > Tolerance And k < k_max
+                        If f1 < f2 Then
+                            a = x1
+                            x1 = x2
+                            f1 = f2
+                            x2 = a + r * (b - a)
+                            f2 = Fd(x2, H)
+                            ValueOfFunction = f2
+                            SolutionOfTask = x2
+                        Else
+                            b = x2
+                            x2 = x1
+                            f2 = f1
+                            x1 = a + (1 - r) * (b - a)
+                            f1 = Fd(x1, H)
+                            ValueOfFunction = f1
+                            SolutionOfTask = x1
+                        End If
+                        k += 1
+                    End While
+
+                    Fx1 = Fd(x1 - Tolerance, H)
+                    Fx2 = Fd(x1 + Tolerance, H)
+
+                    'If cond = 1 Then
+
+                    '    If Sign(YF1 - f1) = Sign(YF1 - f2) Then
+                    '        cond = 0
+                    '        TextBox1.ForeColor = System.Drawing.Color.Green
+                    '        TextBox1.Text = "Результат x* максимайзером этой функции, потому что" & vbCrLf &
+                    '                "Знак [F(x*)-F(x*–Tolerance)]  = " & Sign(YF1 - f1) & vbCrLf &
+                    '                "и" & vbCrLf &
+                    '                "Знак [F(x*)-F(x*+Tolerance)]  = " & Sign(YF1 - f2) & vbCrLf &
+                    '                "имеют одинаковые (плюсовые) знаки" & vbCrLf &
+                    '                "Он был найден с ошибкой H = " & ToleranceBox.Text & vbCrLf &
+                    '                "Это равно заданному Tolerance!"
+
+                    '    Else
+                    '        MessageBox.Show("Задайте начальную точку X(0), так чтобы она была меньше от оптимальной точки (экстремума), для этого нажмите на кнопку Show Function Graph, чтобы увидеть график",
+                    '                        "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    '        x1 = 0
+                    '        YF1 = 0
+                    '        k = 0
+                    '        f1 = 0
+                    '        f2 = 0
+                    '        elapsedSeconds = 0
+                    '        TextBox1.Text = ""
+                    '    End If
+                    'Else
+                    '    If Sign(YF1 - f1) = Sign(YF1 - f2) Then
+                    '        TextBox1.ForeColor = System.Drawing.Color.Green
+                    '        TextBox1.Text = "Результат x* максимайзером этой функции, потому что" & vbCrLf &
+                    '                    "Знак [F(x*) — F(x*–Tolerance)]  = " & Sign(YF1 - f1) & vbCrLf &
+                    '                    "и " & vbCrLf &
+                    '                    "Знак [F(x*) — F(x*+Tolerance)]  = " & Sign(YF1 - f2) & vbCrLf &
+                    '                    "имеют одинаковые (плюсовые) знаки" & vbCrLf &
+                    '                    "Он был найден с ошибкой H = " & ToleranceBox.Text & vbCrLf &
+                    '                    "Это равно заданному Tolerance!"
+                    '    Else
+                    '        TextBox1.ForeColor = System.Drawing.Color.Red
+                    '        TextBox1.Text = "Результат x * не является максимайзером этой функции, потому что " & vbCrLf &
+                    '                    "Знак [F(x*) — F(x*–Tolerance)]  = " & Sign(YF1 - f1) & vbCrLf &
+                    '                    "и" & vbCrLf &
+                    '                    "Знак[F(x*) — F(x*+Tolerance)]  = " & Sign(YF1 - f2) & vbCrLf &
+                    '                    "имеют разные знаки"
+                    '        MsgBox("Эта функция не нашла максимум при заданном количестве итераций, вы должны увеличить Limit of iteration!")
+                    '    End If
+                    'End If
+                Else
+                    MessageBox.Show("Выберите какой экстремум функции вы хотите найти")
+                End If
+            End If
+        Catch ex As ArithmeticException
+            MessageBox.Show("Ошибка, Логорифмическая функция для значения 'X0' должен быть больше 0")
+        Catch ex As Exception
+            MessageBox.Show("Ошибка ввода данных. Убедитесь, что все поля заполнены корректно.")
+        End Try
+        stopwatch.Stop()
+        elapsedSeconds = stopwatch.Elapsed.TotalSeconds
+    End Sub
+
+    Public Sub output(Fplus2 As TextBox, Fminus2 As TextBox, xResultBox As TextBox, funcResultBox As TextBox, NumberOfIterationsBox As TextBox,
+                      TimeBox As TextBox, xminusBox As TextBox, xplusBox As TextBox, TextBox1 As TextBox)
+        If cond = 1 Then
+            xResultBox.Text = ""
+            funcResultBox.Text = ""
+            NumberOfIterationsBox.Text = ""
+            xminusBox.Text = ""
+            xplusBox.Text = ""
+            Fplus2.Text = ""
+            Fminus2.Text = ""
+            TimeBox.Text = ""
+            stopwatch.Reset()
+        Else
+            xResultBox.Text = SolutionOfTask.ToString("N28")
+            xResultBox.ForeColor = System.Drawing.Color.Red
+            funcResultBox.Text = ValueOfFunction.ToString("N28")
+            NumberOfIterationsBox.Text = k
+            xminusBox.Text = Fx1.ToString("0.0e0")
+            xplusBox.Text = Fx2.ToString("0.0e0")
+            Fplus2.Text = (ValueOfFunction - Fx2).ToString("0.0e0")
+            Fminus2.Text = (ValueOfFunction - Fx1).ToString("0.0e0")
+            TimeBox.Text = elapsedSeconds.ToString
+            stopwatch.Reset()
+        End If
+    End Sub
+End Class
